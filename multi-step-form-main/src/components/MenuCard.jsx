dc557b73d1196e73f0a3a8c20a2c62ae44cbcb04
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-31 20:00:56 +0300

    Added Created context and router layout

--- multi-step-form-main/src/components/MenuCard.jsx
+++ multi-step-form-main/src/components/MenuCard.jsx
@@ -1,11 +1,28 @@
+import { useLocation } from 'react-router';
 import PropTypes from 'prop-types';
 
 import './components_style.css';
 
 const MenuCard = ({ menu }) => {
+  const location = useLocation();
+
   return (
     <div className="menu__card">
-      <div className="menu__card--num">{menu.number}</div>
+      <div
+        className={`
+        ${
+          location.pathname === '/' && menu.number === 1
+            ? `menu__card--num_active`
+            : location.pathname === '/step-4' && menu.number === 5
+            ? `menu__card--num_active`
+            : location.pathname === `/step-${menu.number}`
+            ? `menu__card--num_active`
+            : ''
+        }
+        menu__card--num`}
+      >
+        {menu.number}
+      </div>
 
       <div className="menu__card--content">
         <h1>{menu.title}</h1>
