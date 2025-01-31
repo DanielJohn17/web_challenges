93e57753918886093b92b94d04e8a7375391f90f
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-31 22:23:27 +0300

    Integrated subscriptionContext To Step 3

--- multi-step-form-main/src/components/AddOnCard.jsx
+++ multi-step-form-main/src/components/AddOnCard.jsx
@@ -1,16 +1,55 @@
-const AddOnCard = () => {
+import PropType from 'prop-types';
+import { useState } from 'react';
+
+const AddOnCard = ({
+  index,
+  addOn,
+  isYearly,
+  addOnsClicked,
+  handleAddOnsClicked,
+}) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <div className="addon__card">
-      <input type="checkbox" />
+    <div
+      className="addon__card"
+      onClick={() => {
+        handleAddOnsClicked(index, addOn, isYearly);
+        setIsChecked((prev) => !prev);
+      }}
+      style={
+        addOnsClicked.some((item) => item.index === index) && isChecked
+          ? {
+              borderColor: 'var(--purplish-blue)',
+              backgroundColor: 'hsla(228, 100%, 84%, 0.05)',
+            }
+          : {}
+      }
+    >
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked((prev) => !prev)}
+      />
 
       <div className="addon__card--content">
-        <h3>Online Service</h3>
-        <p>Access to multiplayer games</p>
+        <h3>{addOn.name}</h3>
+        <p>{addOn.description}</p>
       </div>
 
-      <div className="addon__card--price">+$1/mo</div>
+      <div className="addon__card--price">
+        {isYearly ? addOn.price.yearly : addOn.price.monthly}
+      </div>
     </div>
   );
 };
 
 export default AddOnCard;
+
+AddOnCard.propTypes = {
+  index: PropType.number.isRequired,
+  addOn: PropType.object.isRequired,
+  isYearly: PropType.bool.isRequired,
+  addOnsClicked: PropType.array.isRequired,
+  handleAddOnsClicked: PropType.func.isRequired,
+};
--- multi-step-form-main/src/container/StepThree.jsx
+++ multi-step-form-main/src/container/StepThree.jsx
@@ -1,8 +1,64 @@
+import { useState, useContext } from 'react';
+import { subscriptionContext } from '../context/context';
+
 import AddOnCard from '../components/AddOnCard';
 import StepHeader from '../components/StepHeader';
 import Button from '../components/Button';
 
+const addOns = [
+  {
+    name: 'Online Service',
+    description: 'Access to multiplyer games',
+    price: {
+      monthly: '$1/mo',
+      yearly: '$10/yr',
+    },
+  },
+  {
+    name: 'Large Storage',
+    description: 'Extra 1TB of cloud save',
+    price: {
+      monthly: '$2/mo',
+      yearly: '$20/yr',
+    },
+  },
+  {
+    name: 'Customizable profile',
+    description: 'Custom theme on your profile',
+    price: {
+      monthly: '$2/mo',
+      yearly: '$20/yr',
+    },
+  },
+];
+
 const StepThree = () => {
+  const { isYearly, updateSubscriptionData, handleBackStep, handleNextStep } =
+    useContext(subscriptionContext);
+  const [addOnsClicked, setAddOnsClicked] = useState([]);
+
+  const handleAddOnsClicked = (index, addOn, isYearly) => {
+    if (isYearly) {
+      setAddOnsClicked((prevAddOns) => [
+        ...prevAddOns,
+        { index, name: addOn.name, payment: addOn.price.yearly },
+      ]);
+    } else {
+      setAddOnsClicked((prevAddOns) => [
+        ...prevAddOns,
+        { index, name: addOn.name, payment: addOn.price.monthly },
+      ]);
+    }
+  };
+
+  const handleNext = () => {
+    updateSubscriptionData({ addOns: addOnsClicked });
+    handleNextStep('/step-4');
+  };
+
+  const handleBack = () => {
+    handleBackStep('/step-2');
+  };
   return (
     <div className="step__container">
       <StepHeader
@@ -11,14 +67,25 @@ const StepThree = () => {
       />
 
       <div className="step__container--add">
-        <AddOnCard />
-        <AddOnCard />
-        <AddOnCard />
+        {addOns.map((addOn, index) => (
+          <AddOnCard
+            key={index}
+            index={index}
+            addOn={addOn}
+            isYearly={isYearly}
+            addOnsClicked={addOnsClicked}
+            handleAddOnsClicked={handleAddOnsClicked}
+          />
+        ))}
       </div>
 
       <div className="step__container--buttons">
-        <Button type="back">Go Back</Button>
-        <Button type="next">Next Step</Button>
+        <Button type="back" handleClick={handleBack}>
+          Go Back
+        </Button>
+        <Button type="next" handleClick={handleNext}>
+          Next Step
+        </Button>
       </div>
     </div>
   );
