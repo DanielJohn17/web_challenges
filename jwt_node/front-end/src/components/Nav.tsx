2011a02974f9221ee3ec60bc67c87df1ad8f9ece
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-23 22:45:54 +0300

    Button and nav bar initial setup

---
+++ jwt_node/front-end/src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import Button from './Button';
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>Hello, name</h1>
+
+      <div>
+        <Button />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
