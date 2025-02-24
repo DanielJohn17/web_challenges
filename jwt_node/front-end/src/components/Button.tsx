7d3383d396bffbb0cd3b5b62ef4e55cfedf8b72a
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-24 22:11:13 +0300

    Custom Button

--- jwt_node/front-end/src/components/Button.tsx
+++ jwt_node/front-end/src/components/Button.tsx
@@ -1,5 +1,44 @@
-const Button = () => {
-  return <button>Click me!</button>;
+import { Button } from '@mui/material';
+type CustomButtonProps = {
+  type: 'login' | 'register' | 'logout';
 };
 
-export default Button;
+const CustomButton = ({ type }: CustomButtonProps) => {
+  if (type === 'login') {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ padding: '15px 0', fontSize: '1.1rem' }}
+      >
+        Login
+      </Button>
+    );
+  }
+
+  if (type === 'register') {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ padding: '15px 0', fontSize: '1.1rem' }}
+      >
+        Register
+      </Button>
+    );
+  }
+
+  if (type === 'logout') {
+    return (
+      <Button
+        variant="contained"
+        color="error"
+        sx={{ padding: '15px 0', fontSize: '1.1rem' }}
+      >
+        Logout
+      </Button>
+    );
+  }
+};
+
+export default CustomButton;
