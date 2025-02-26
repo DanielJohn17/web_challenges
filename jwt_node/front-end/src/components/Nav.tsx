15b345f5ed48db104bfa71f94963e35542d21b2d
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-26 22:16:52 +0300

    Finished Navigation

--- jwt_node/front-end/src/components/Nav.tsx
+++ jwt_node/front-end/src/components/Nav.tsx
@@ -1,14 +1,48 @@
-import Button from './Button';
+import { Button } from '@mui/material';
+import { NavLink } from 'react-router';
+
+type CustomLinkProps = {
+  to: string;
+  children: React.ReactNode;
+};
 
 const Nav = () => {
   return (
-    <nav>
-      <h1>Hello, name</h1>
+    <nav className="w-full h-48 bg-custom-blue flex justify-between items-center px-14 text-white">
+      <h1 className="text-5xl font-semibold">
+        Hello, <span className="font-light">name</span>
+      </h1>
 
-      <div>
-        <Button />
+      <ul className="flex gap-14">
+        <li className="text-2xl">
+          <CustomLink to={'/'}>Me</CustomLink>
+        </li>
+        <li className="text-2xl">
+          <CustomLink to={'/users'}>Users</CustomLink>
+        </li>
+      </ul>
+
+      <div className="w-3xs flex">
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ width: '100%', fontSize: '1.1rem', color: 'white' }}
+        >
+          Logout
+        </Button>
       </div>
     </nav>
+  );
+};
+
+const CustomLink = ({ to, children }: CustomLinkProps) => {
+  return (
+    <NavLink
+      to={to}
+      className="text-2xl hover:text-custom-gray transition-colors duration-300"
+    >
+      {children}
+    </NavLink>
   );
 };
 
