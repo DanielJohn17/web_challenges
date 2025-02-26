512f27d1d5d06ebf6f964a695d7f7725ce69f0ff
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-26 22:17:19 +0300

    Integrated routers

--- jwt_node/front-end/src/App.tsx
+++ jwt_node/front-end/src/App.tsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import MePage from './pages/Me';
+import Users from './pages/Users';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />;
+    </BrowserRouter>
+  );
 }
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}>
+        <Route index element={<MePage />} />
+        <Route path="users" element={<Users />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
 
 export default App;
