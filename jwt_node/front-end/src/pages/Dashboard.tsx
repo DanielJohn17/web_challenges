2c1b74280c25c4c14adb2f29f1fdad3761707eef
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-27 16:47:23 +0300

    Finished Dashboard

--- jwt_node/front-end/src/pages/Dashboard.tsx
+++ jwt_node/front-end/src/pages/Dashboard.tsx
@@ -2,19 +2,46 @@ import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router';
 import Cookies from 'js-cookie';
 import Nav from '../components/Nav';
+import { User } from '../context/UserContext';
 
-const Dashboard = () => {
+type DashboardProps = {
+  setUser: (user: User) => void;
+};
+
+const Dashboard = ({ setUser }: DashboardProps) => {
   const navigate = useNavigate();
+
   useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/user/me', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        const data = await response.json();
+
+        setUser(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) console.log(`Error: ${error.message}`);
+        else console.log('Unkown Error');
+      }
+    };
+
     const getToken = () => {
       const token = Cookies.get('auth_cookie');
       if (!token) {
-        // navigate('/login');
+        navigate('/login');
+      } else {
+        getUserData();
       }
     };
 
     getToken();
-  }, [navigate]);
+  }, [navigate, setUser]);
   return (
     <div className="min-h-screen w-screen flex flex-col gap-24">
       <Nav />
