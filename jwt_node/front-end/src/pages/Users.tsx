5a5f04e09ee7358c697137b6be1756e4e4200749
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-27 16:49:07 +0300

    Finished Users page

--- jwt_node/front-end/src/pages/Users.tsx
+++ jwt_node/front-end/src/pages/Users.tsx
@@ -7,8 +7,39 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { User } from '../context/UserContext';
 
 const Users = () => {
+  const [rows, setRows] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/user/all', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+        setRows(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) console.log(`Error: ${error.message}`);
+        else console.log('Unkown Error');
+      }
+    };
+
+    fetchUsers();
+  }, []);
   return (
     <div className="w-full h-full flex items-center justify-center">
       <TableContainer component={Paper} sx={{ maxWidth: 900 }}>
@@ -16,22 +47,22 @@ const Users = () => {
           <TableHead>
             <TableRow>
               <TableCell>Full Name</TableCell>
-              <TableCell align="right">Email</TableCell>
-              <TableCell align="right">Date Of Birth</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Role</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))} */}
+            {rows &&
+              rows?.length > 0 &&
+              rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.role}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
