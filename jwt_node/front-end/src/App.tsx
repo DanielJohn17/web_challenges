e32f8d0fc54cd142323bff7f547c214c638ca38f
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-27 16:46:34 +0300

    Added user context and code cleanup

--- jwt_node/front-end/src/App.tsx
+++ jwt_node/front-end/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { lazy, useState } from 'react';
+import { User, UserContext } from './context/UserContext';
+
 const Login = lazy(() => import('./pages/Login'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const Register = lazy(() => import('./pages/Register'));
@@ -7,24 +9,21 @@ const MePage = lazy(() => import('./pages/Me'));
 const Users = lazy(() => import('./pages/Users'));
 
 function App() {
+  const [user, setUser] = useState<User | undefined>(undefined);
   return (
-    <BrowserRouter>
-      <AppRoutes />;
-    </BrowserRouter>
+    <UserContext.Provider value={user}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard setUser={setUser} />}>
+            <Route index element={<MePage />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
-
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route index element={<MePage />} />
-        <Route path="users" element={<Users />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  );
-};
 
 export default App;
